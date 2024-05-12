b0f682df656f3e5fc8786d5860cc23fff751cfc5
Nastia Bondar <145715374+Nastiia18@users.noreply.github.com>
2024-05-13 00:48:45 +0300

    Add files via upload

---
+++ №9-Варіант5.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// Абстрактний клас заводу
+abstract class Factory
+{
+    public abstract void Produce();
+}
+
+// Конкретний клас заводу
+class ConcreteFactory : Factory
+{
+    public override void Produce()
+    {
+        Console.WriteLine("Виробництво цивільного обладнання...");
+    }
+}
+
+// Декоратор для виробництва військового обладнання
+class MilitaryEquipmentDecorator : Factory
+{
+    private Factory factory;
+
+    public MilitaryEquipmentDecorator(Factory factory)
+    {
+        this.factory = factory;
+    }
+
+    public override void Produce()
+    {
+        factory.Produce(); // Викликаємо метод виробництва базового заводу
+        Console.WriteLine("Додаємо виробництво військового обладнання...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо базовий завод
+        Factory factory = new ConcreteFactory();
+
+        // Додаємо декоратор для виробництва військового обладнання
+        Factory militaryFactory = new MilitaryEquipmentDecorator(factory);
+
+        // Запускаємо виробництво
+        militaryFactory.Produce();
+    }
+}
+
